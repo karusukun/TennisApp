ac9cc4b30cc6e9af0bd46d0d2afc962d22801590
ecruz1794 <ecruz1794@gmail.com>
2014-04-25 20:16:44 -0600

    Reporte

--- TennisApp/src/Logic/Report.java
+++ TennisApp/src/Logic/Report.java
@@ -1,12 +1,23 @@
 package Logic;
 
-public class Report {
+import Library.modePaint;
+import java.util.Date;
 
-    //Report's atributtes
-            private String string__designName;
-            private String float__arcadeTime;
-            private String float__fireTime;
-            private String date__reportDate;
+public class Report {
+    
+    public void createReport(modePaint pMode, float pTime){
+        Date date = new Date();
+        date__reportDate = date.toString();
+        string__designName = DesignLogic.getDesignLogicInstance().getActualDesign().getName();
+        
+        if(pMode == modePaint.Arcade){
+            float__arcadeTime = Float.toString(pTime);
+        }if(pMode == modePaint.Fire){
+            float__fireTime =  Float.toString(pTime);
+        }if(bestTimeExecution > pTime){
+            bestTimeExecution = pTime;
+        }
+    }
 
     public String getString__designName() {
         return string__designName;
@@ -48,6 +59,21 @@ public class Report {
         this.tennisPaintLogic = tennisPaintLogic;
     }
 
+    //Method
+    public float getBestTimeExecution() {
+        return bestTimeExecution;
+    }
+
+    public void setBestTimeExecution(float bestTimeExecution) {
+        this.bestTimeExecution = bestTimeExecution;
+    }
+    
     private TennisPaintLogic tennisPaintLogic;
+    //Report's atributtes
+    private float bestTimeExecution;
+    private String string__designName;
+    private String float__arcadeTime;
+    private String float__fireTime;
+    private String date__reportDate;
 
 }
